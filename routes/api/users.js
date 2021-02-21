ec1f9cb3ffcd7d3f61c214bdcde892f7c5336602
shubham-pal-au9 <spal5776@gmail.com>
2021-02-21 16:34:30 +0530

    Setup-routes

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route GET /api Users
+// @desc  Test route
+// @access public
+router.get('/',(req,res) => res.send('User Route'));
+
+module.exports = router;
